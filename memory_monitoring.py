140fd6dd0282f0155f4becce8742e0876b4993c9
yeon <yeon@vaultmicro.com>
2020-10-27 11:35:43 +0900

    update

--- memory_monitoring.py
+++ memory_monitoring.py
@@ -2,6 +2,11 @@ import matplotlib.pyplot as plt
 import subprocess
 import time
 
+# 모니터링이 필요한 패키지 이름
+# 여기에 모니터링이 필요한 패키지 이름을 입력해주세요
+# ex) com.example.test
+packageName = "com.example.test"
+
 # 테스트 시작 시간
 startTime = time.time()
 
@@ -21,7 +26,7 @@ while True:
     # tr -d ' ' : 공백 제거
     # awk -F " " '{ split($0, array, "K");print array[1]; }' : K를 이용한 문자열 자르기
     #  >> meminfo.txt : meminfo.txt에 결과 문자열 write
-    cmd = ['adb','shell','dumpsys','meminfo','|','grep','com.vaultmicro.camerafi.live','|','xargs echo','|','tr','-d','\',\'','|','tr','-d','\' \'','|','awk','-F','" "','\'{ split($0, array, "K");print array[1]; }\'']
+    cmd = ['adb','shell','dumpsys','meminfo','|','grep',packageName,'|','xargs echo','|','tr','-d','\',\'','|','tr','-d','\' \'','|','awk','-F','" "','\'{ split($0, array, "K");print array[1]; }\'']
     # 명령어 실행 후 반환되는 결과를 파일에 저장
     fd_popen = subprocess.Popen(cmd,stdout=subprocess.PIPE).stdout
     # 파일에 저장된 결과를 읽고 utf-8로 인코딩
