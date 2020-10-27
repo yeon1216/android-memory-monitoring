04dd1c779539a0e3d48ab23c1bc9bd6b81f0b699
yeon1216 <37677444+yeon1216@users.noreply.github.com>
2020-10-27 11:46:34 +0900

    Update meminfo.sh

--- meminfo.sh
+++ meminfo.sh
@@ -7,12 +7,12 @@ timestamp=`date +%Y%m%d%H%M%S`
 #echo `date +%Y%m%d%H%M` >> meminfo.txt
 
 # adb shell dumpsys meminfo : 안드로이드폰에서 실행중인 프로세스 메모리 정보 read
-# grep com.vaultmicro.camerafi.live : com.vaultmicro.camerafi.live와 일치하는 부분 find
+# grep [packageName] : [packageName]와 일치하는 부분 find
 # xargs echo : 결과물을 한줄로 만들어줌
 # tr -d ',' : , 문자열 제거
 # tr -d ' ' : 공백 제거
 # awk -F " " '{ split($0, array, "K");print array[1]; }' : K를 이용한 문자열 자르기
 #  >> meminfo.txt : meminfo.txt에 결과 문자열 write
-tempMemory=`adb shell dumpsys meminfo | grep com.vaultmicro.camerafi.live | xargs echo | tr -d ',' | tr -d ' ' | awk -F " " '{ split($0, array, "K");print array[1]; }'`
+tempMemory=`adb shell dumpsys meminfo | grep [packageName] | xargs echo | tr -d ',' | tr -d ' ' | awk -F " " '{ split($0, array, "K");print array[1]; }'`
 
 echo "$timestamp,$tempMemory" >> meminfo.txt
